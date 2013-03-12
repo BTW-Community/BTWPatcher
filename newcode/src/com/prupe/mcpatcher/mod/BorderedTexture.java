746613a5aa67fbab95bb425b07a001f5c8397381
Paul Rupe <prupe@nc.rr.com>
2013-03-12 19:46:28 -0400

    implement TextureStitched override methods

--- newcode/src/com/prupe/mcpatcher/mod/BorderedTexture.java
+++ newcode/src/com/prupe/mcpatcher/mod/BorderedTexture.java
@@ -6,52 +6,60 @@ import net.minecraft.src.TextureStitched;
 import java.util.List;
 
 public class BorderedTexture extends TextureStitched {
+    private float normalizedX0;
+    private float normalizedX1;
+    private float normalizedY0;
+    private float normalizedY1;
+    private float normalizedWidth;
+    private float normalizedHeight;
+
     public BorderedTexture(String name) {
         super(name);
     }
 
     @Override
-    public void setup(Texture texture, List subTextures, int x0, int y0, int width, int height, boolean flipped) {
+    public void setup(Texture texture, List<Texture> subTextures, int x0, int y0, int width, int height, boolean flipped) {
         super.setup(texture, subTextures, x0, y0, width, height, flipped);
-    }
-
-    @Override
-    public int getX0() {
-        return 0;
-    }
-
-    @Override
-    public int getY0() {
-        return 0;
+        int border = subTextures == null || subTextures.isEmpty() ? 0 : subTextures.get(0).border;
+        x0 += border;
+        y0 += border;
+        width -= 2 * border;
+        height -= 2 * border;
+        normalizedX0 = (float) x0 / (float) texture.getWidth();
+        normalizedX1 = (float) (x0 + width) / (float) texture.getWidth();
+        normalizedY0 = (float) y0 / (float) texture.getHeight();
+        normalizedY1 = (float) (y0 + height) / (float) texture.getHeight();
+        normalizedWidth = normalizedX1 - normalizedX0;
+        normalizedHeight = normalizedY1 - normalizedY0;
     }
 
     @Override
     public float getNormalizedX0() {
-        return 0;
+        return normalizedX0;
     }
 
     @Override
     public float getNormalizedX1() {
-        return 0;
+        return normalizedX1 - Float.MIN_VALUE;
     }
 
     @Override
-    public float interpolateX(double var1) {
-        return 0;
+    public float interpolateX(double x) {
+        return normalizedX0 + ((float) x / 16.0f) * normalizedWidth - Float.MIN_VALUE;
     }
 
     @Override
     public float getNormalizedY0() {
-        return 0;
+        return normalizedY0;
     }
 
     @Override
     public float getNormalizedY1() {
-        return 0;
+        return normalizedY1 - Float.MIN_VALUE;
     }
 
     @Override
-    public float interpolateY(double var1) {
-        return 0;
+    public float interpolateY(double y) {
+        return normalizedY0 + ((float) y / 16.0f) * normalizedHeight - Float.MIN_VALUE;
     }
 }
