a3a10ce9bee7150d65a7a8eff8c77d4cb5bc5cc7
Paul Rupe <prupe@nc.rr.com>
2013-03-14 18:42:58 -0400

    use vanilla TextureStitched coordinates unless aa/aniso is enabled

--- newcode/src/com/prupe/mcpatcher/mod/BorderedTexture.java
+++ newcode/src/com/prupe/mcpatcher/mod/BorderedTexture.java
@@ -10,8 +10,9 @@ public class BorderedTexture extends TextureStitched {
     private float normalizedX1;
     private float normalizedY0;
     private float normalizedY1;
-    private float normalizedWidth;
-    private float normalizedHeight;
+    private float scaledWidth;
+    private float scaledHeight;
+    private int border;
 
     public BorderedTexture(String name) {
         super(name);
@@ -20,19 +21,24 @@ public class BorderedTexture extends TextureStitched {
     @Override
     public void setup(Texture texture, List<Texture> subTextures, int x0, int y0, int width, int height, boolean flipped) {
         super.setup(texture, subTextures, x0, y0, width, height, flipped);
-        int border = subTextures == null || subTextures.isEmpty() ? 0 : subTextures.get(0).border;
-        x0 += border;
-        y0 += border;
-        width -= 2 * border;
-        height -= 2 * border;
-        normalizedX0 = (float) x0 / (float) texture.getWidth();
-        normalizedX1 = (float) (x0 + width) / (float) texture.getWidth();
-        normalizedY0 = (float) y0 / (float) texture.getHeight();
-        normalizedY1 = (float) (y0 + height) / (float) texture.getHeight();
-        normalizedWidth = normalizedX1 - normalizedX0;
-        normalizedHeight = normalizedY1 - normalizedY0;
-        normalizedX1 = minusEpsilon(normalizedX1);
-        normalizedY1 = minusEpsilon(normalizedY1);
+        border = subTextures == null || subTextures.isEmpty() ? 0 : subTextures.get(0).border;
+        if (border > 0) {
+            x0 += border;
+            y0 += border;
+            width -= 2 * border;
+            height -= 2 * border;
+            normalizedX0 = (float) x0 / (float) texture.getWidth();
+            normalizedX1 = (float) (x0 + width) / (float) texture.getWidth();
+            normalizedY0 = (float) y0 / (float) texture.getHeight();
+            normalizedY1 = (float) (y0 + height) / (float) texture.getHeight();
+        } else {
+            normalizedX0 = super.getNormalizedX0();
+            normalizedX1 = super.getNormalizedX1();
+            normalizedY0 = super.getNormalizedY0();
+            normalizedY1 = super.getNormalizedY1();
+        }
+        scaledWidth = (normalizedX1 - normalizedX0) / 16.0f;
+        scaledHeight = (normalizedY1 - normalizedY0) / 16.0f;
     }
 
     @Override
@@ -47,7 +53,7 @@ public class BorderedTexture extends TextureStitched {
 
     @Override
     public float interpolateX(double x) {
-        return minusEpsilon(normalizedX0 + ((float) x / 16.0f) * normalizedWidth);
+        return border > 0 ? normalizedX0 + (float) x * scaledWidth : super.interpolateX(x);
     }
 
     @Override
@@ -62,14 +68,6 @@ public class BorderedTexture extends TextureStitched {
 
     @Override
     public float interpolateY(double y) {
-        return minusEpsilon(normalizedY0 + ((float) y / 16.0f) * normalizedHeight);
-    }
-
-    private static float minusEpsilon(float f) {
-        if (f > 0.0f) {
-            return Float.intBitsToFloat(Float.floatToRawIntBits(f) - 1);
-        } else {
-            return f;
-        }
+        return border > 0 ? normalizedY0 + (float) y * scaledHeight : super.interpolateY(y);
     }
 }
