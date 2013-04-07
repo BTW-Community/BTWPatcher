787b6eac19c1e3c4967119c7a79f1d7e433050fb
Paul Rupe <prupe@nc.rr.com>
2013-04-07 11:29:17 -0400

    additional overlay properties

--- newcode/src/com/prupe/mcpatcher/mod/ItemOverlay.java
+++ newcode/src/com/prupe/mcpatcher/mod/ItemOverlay.java
@@ -10,12 +10,21 @@ import org.lwjgl.opengl.GL11;
 import java.util.Properties;
 
 class ItemOverlay {
+    static final int AVERAGE = 0;
+    static final int CYCLE = 1;
+    static final int TOP = 2;
+
     private static final float ITEM_2D_THICKNESS = 0.0625f;
 
     private final String texture;
     private final BlendMethod blendMethod;
-    private final float rotate;
+    private final float rotation;
     private final float speed;
+    final float duration;
+    final int weight;
+    final int applyMethod;
+    final int limit;
+    final int group;
 
     static void beginOuter2D() {
         GL11.glEnable(GL11.GL_BLEND);
@@ -56,16 +65,43 @@ class ItemOverlay {
             override.error("unknown blend type %s", value);
             return null;
         }
-        float rotate = MCPatcherUtils.getFloatProperty(properties, "rotate", 0.0f);
+        float rotate = MCPatcherUtils.getFloatProperty(properties, "rotation", 0.0f);
         float speed = MCPatcherUtils.getFloatProperty(properties, "speed", 0.0f);
-        return new ItemOverlay(override.textureName, blendMethod, rotate, speed);
+        float duration = MCPatcherUtils.getFloatProperty(properties, "duration", 1.0f);
+        int weight = MCPatcherUtils.getIntProperty(properties, "weight", 1);
+        int method;
+        String[] tokens = MCPatcherUtils.getStringProperty(properties, "apply", "average").toLowerCase().split("\\s+");
+        if (tokens[0].equals("average")) {
+            method = AVERAGE;
+        } else if (tokens[0].equals("top")) {
+            method = TOP;
+        } else if (tokens[0].equals("cycle")) {
+            method = CYCLE;
+        } else {
+            override.error("unknown apply type %s", tokens[0]);
+            return null;
+        }
+        int limit = 0;
+        if (tokens.length > 1) {
+            try {
+                limit = Integer.parseInt(tokens[1]);
+            } catch (NumberFormatException e) {
+            }
+        }
+        limit = Math.max(Math.min(limit, CITUtils.MAX_ENCHANTMENTS - 1), 0);
+        return new ItemOverlay(override.textureName, blendMethod, rotate, speed, duration, weight, method, limit);
     }
 
-    ItemOverlay(String texture, BlendMethod blendMethod, float rotate, float speed) {
+    ItemOverlay(String texture, BlendMethod blendMethod, float rotation, float speed, float duration, int weight, int applyMethod, int limit) {
         this.texture = texture;
         this.blendMethod = blendMethod;
-        this.rotate = rotate;
+        this.rotation = rotation;
         this.speed = speed;
+        this.duration = duration;
+        this.weight = weight;
+        this.applyMethod = applyMethod;
+        this.limit = limit;
+        group = (limit << 2) | applyMethod;
     }
 
     void render2D(Tessellator tessellator, float fade, float x0, float y0, float x1, float y1, float z) {
@@ -128,7 +164,7 @@ class ItemOverlay {
             float offset = (float) (System.currentTimeMillis() % 3000L) / 3000.0f * 8.0f;
             GL11.glTranslatef(offset * speed, 0.0f, 0.0f);
         }
-        GL11.glRotatef(rotate, 0.0f, 0.0f, 1.0f);
+        GL11.glRotatef(rotation, 0.0f, 0.0f, 1.0f);
     }
 
     private void end() {
