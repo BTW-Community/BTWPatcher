06457a8658310df9c00e9ad470ad90972256e917
Paul Rupe <prupe@nc.rr.com>
2013-04-22 09:56:09 -0400

    add fontHeight field to stub

--- stubs/src/net/minecraft/src/FontRenderer.java
+++ stubs/src/net/minecraft/src/FontRenderer.java
@@ -1,6 +1,8 @@
 package net.minecraft.src;
 
 public class FontRenderer {
+    public int fontHeight = 9;
+
     public float[] charWidthf; // added by HD Font
 
     public int getCharWidth(char c) { // 1.2.4 and up
