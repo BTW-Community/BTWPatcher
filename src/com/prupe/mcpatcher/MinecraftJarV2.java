48a148ed24bc7fcf4eb354dd89d10e70c8ee9ac2
Paul Rupe <prupe@nc.rr.com>
2013-04-22 20:59:37 -0400

    parse .json file to build classpath

--- src/com/prupe/mcpatcher/MinecraftJarV2.java
+++ src/com/prupe/mcpatcher/MinecraftJarV2.java
@@ -1,6 +1,8 @@
 package com.prupe.mcpatcher;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
@@ -47,10 +49,49 @@ class MinecraftJarV2 extends MinecraftJarBase {
 
     @Override
     void addToClassPath(List<File> classPath) {
-        File libDir = MCPatcherUtils.getMinecraftPath("libraries");
-        if (libDir.isDirectory()) {
-            addToClassPath(libDir, classPath);
+        if (!getClassPathFromJSON(classPath)) {
+            File libDir = MCPatcherUtils.getMinecraftPath("libraries");
+            if (libDir.isDirectory()) {
+                addToClassPath(libDir, classPath);
+            }
         }
+    }
+
+    private boolean getClassPathFromJSON(List<File> classPath) {
+        BufferedReader reader = null;
+        try {
+            String v = getVersion().getVersionString();
+            File json = MCPatcherUtils.getMinecraftPath("versions", v, v + ".json");
+            if (!json.isFile()) {
+                Logger.log(Logger.LOG_JAR, "WARNING: %s not found", json);
+                return false;
+            }
+            reader = new BufferedReader(new FileReader(json));
+            boolean foundLibs = false;
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.split("[ \t:\"]+");
+                if (tokens.length >= 2 && tokens[1].equals("libraries")) {
+                    foundLibs = true;
+                } else if (foundLibs && tokens.length >= 5 && tokens[1].equals("name")) {
+                    String subdir = tokens[2].replace('.', '/');
+                    String base = tokens[3];
+                    String version = tokens[4];
+                    File jar = MCPatcherUtils.getMinecraftPath("libraries", subdir, base, version, base + '-' + version + ".jar");
+                    if (jar.isFile()) {
+                        classPath.add(jar);
+                    } else {
+                        Logger.log(Logger.LOG_JAR, "WARNING: %s not found", jar);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            MCPatcherUtils.close(reader);
+        }
+        return true;
     }
 
     private static void addToClassPath(File dir, List<File> classPath) {
