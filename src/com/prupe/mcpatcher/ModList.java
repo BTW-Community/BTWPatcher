c8c58ac5d1e60e4b4eb603d4080729b848df96e1
Paul Rupe <prupe@nc.rr.com>
2013-05-04 12:37:16 -0400

    make all mods visible temporarily

--- src/com/prupe/mcpatcher/ModList.java
+++ src/com/prupe/mcpatcher/ModList.java
@@ -43,9 +43,9 @@ class ModList {
     }
 
     private static BuiltInMod[] builtInMods = new BuiltInMod[]{
-        new BuiltInMod(BaseMod.NAME, BaseMod.class, true, false),
-        new BuiltInMod(BaseTexturePackMod.NAME, BaseTexturePackMod.class, true, false),
-        new BuiltInMod(BaseTilesheetMod.NAME, BaseTilesheetMod.class, true, false),
+        new BuiltInMod(BaseMod.NAME, BaseMod.class, false, false),
+        new BuiltInMod(BaseTexturePackMod.NAME, BaseTexturePackMod.class, false, false),
+        new BuiltInMod(BaseTilesheetMod.NAME, BaseTilesheetMod.class, false, false),
         new BuiltInMod(MCPatcherUtils.EXTENDED_HD, ExtendedHD.class, false, false),
         new BuiltInMod(MCPatcherUtils.RANDOM_MOBS, RandomMobs.class, false, false),
         new BuiltInMod(MCPatcherUtils.CUSTOM_COLORS, CustomColors.class, false, false),
