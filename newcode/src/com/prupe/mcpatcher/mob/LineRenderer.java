a7a61636a8b9724651249048018e70692216ea27
Paul Rupe <prupe@nc.rr.com>
2013-05-20 19:48:27 -0400

    draw arc to mimic vanilla rendering

--- newcode/src/com/prupe/mcpatcher/mob/LineRenderer.java
+++ newcode/src/com/prupe/mcpatcher/mob/LineRenderer.java
@@ -10,17 +10,17 @@ public class LineRenderer {
     private static final MCLogger logger = MCLogger.getLogger(MCPatcherUtils.RANDOM_MOBS);
 
     private static final boolean enable = Config.getBoolean(MCPatcherUtils.RANDOM_MOBS, "leashLine", true);
-    private static final LineRenderer[] renderers = new LineRenderer[2];
+    private static final LineRenderer[] renderers = new LineRenderer[] {
+        new LineRenderer("fishing_line", 0.0075, 0.0, 0.0, 16),
+        new LineRenderer("lead", 0.025, 4.0 / 3.0, 0.125, 24),
+    };
 
     private final String texture;
-    private final float width;
+    private final double width;
+    private final double a;
+    private final double b;
     private final int segments;
     private boolean active;
-
-    static {
-        new LineRenderer(0, "fishing_line", 0.0075f, 16);
-        new LineRenderer(1, "lead", 0.025f, 24);
-    }
 
     public static boolean renderLine(int type, double x, double y, double z, double dx, double dy, double dz) {
         return renderers[type].render(x, y, z, dx, dy, dz);
@@ -32,11 +32,12 @@ public class LineRenderer {
         }
     }
 
-    LineRenderer(int type, String name, float width, int segments) {
+    LineRenderer(String name, double width, double a, double b, int segments) {
         texture = MCPatcherUtils.TEXTURE_PACK_PREFIX + "item/" + name + ".png";
         this.width = width;
+        this.a = a;
+        this.b = b;
         this.segments = segments;
-        renderers[type] = this;
     }
 
     void reset1() {
@@ -55,10 +56,30 @@ public class LineRenderer {
         TexturePackAPI.bindTexture(texture);
         Tessellator tessellator = Tessellator.instance;
         tessellator.startDrawingQuads();
-        tessellator.addVertexWithUV(x, y, z, 0.0, 0.0);
-        tessellator.addVertexWithUV(x + dx, y + dy, z + dz, 1.0, 0.0);
-        tessellator.addVertexWithUV(x + dx, y + dy + width, z + dz, 1.0, 1.0);
-        tessellator.addVertexWithUV(x, y + width, z, 0.0, 1.0);
+        double x0 = x;
+        double y0 = y;
+        double z0 = z;
+        for (int i = 1; i <= segments; i++) {
+            double s = i / (double) segments;
+            double x1 = x + s * dx;
+            double y1 = y + (s * s + s) * dy * 0.5 + a * (1.0 - s) + b;
+            double z1 = z + s * dz;
+
+            tessellator.addVertexWithUV(x0, y0, z0, 0.0, 0.0);
+            tessellator.addVertexWithUV(x1, y1, z1, 1.0, 0.0);
+            tessellator.addVertexWithUV(x1, y1 + width, z1, 1.0, 1.0);
+            tessellator.addVertexWithUV(x0, y0 + width, z0, 0.0, 1.0);
+
+            tessellator.addVertexWithUV(x0, y0 + width, z0, 0.0, 1.0);
+            tessellator.addVertexWithUV(x1, y1 + width, z1, 1.0, 1.0);
+            tessellator.addVertexWithUV(x1, y1, z1, 1.0, 0.0);
+            tessellator.addVertexWithUV(x0, y0, z0, 0.0, 0.0);
+
+            x0 = x1;
+            y0 = y1;
+            z0 = z1;
+
+        }
         tessellator.draw();
         return true;
     }
