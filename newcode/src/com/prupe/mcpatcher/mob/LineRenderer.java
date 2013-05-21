5d93072f4c6f86294e129da862b38dde0885675a
Paul Rupe <prupe@nc.rr.com>
2013-05-20 20:18:58 -0400

    fully init LineRenderer in constructor

--- newcode/src/com/prupe/mcpatcher/mob/LineRenderer.java
+++ newcode/src/com/prupe/mcpatcher/mob/LineRenderer.java
@@ -14,58 +14,49 @@ public class LineRenderer {
     private static final String LINE_PROPERTIES = MCPatcherUtils.TEXTURE_PACK_PREFIX + "item/line.properties";
 
     private static final boolean enable = Config.getBoolean(MCPatcherUtils.RANDOM_MOBS, "leashLine", true);
-    private static final LineRenderer[] renderers = new LineRenderer[] {
-        new LineRenderer("fishingLine", 0.0075, 0.0, 0.0, 16),
-        new LineRenderer("lead", 0.025, 4.0 / 3.0, 0.125, 24),
-    };
+    private static final LineRenderer[] renderers = new LineRenderer[2];
 
-    private final String name;
     private final String texture;
-    private final double defaultWidth;
+    private final double width;
     private final double a;
     private final double b;
     private final int segments;
-
-    private boolean active;
-    private double width;
+    private final boolean active;
 
     public static boolean renderLine(int type, double x, double y, double z, double dx, double dy, double dz) {
-        return renderers[type].render(x, y, z, dx, dy, dz);
+        LineRenderer renderer = renderers[type];
+        return renderer != null && renderer.render(x, y, z, dx, dy, dz);
     }
 
     static void reset() {
-        for (LineRenderer renderer : renderers) {
-            renderer.reset1();
-            if (renderer.active) {
-                logger.fine("using %s", renderer);
-            } else {
-                logger.fine("%s not found", renderer);
-            }
+        if (enable) {
+            setup(0, "fishingLine", 0.0075, 0.0, 0.0, 16);
+            setup(1, "lead", 0.025, 4.0 / 3.0, 0.125, 24);
         }
     }
 
-    LineRenderer(String name, double defaultWidth, double a, double b, int segments) {
-        this.name = name;
+    private static void setup(int type, String name, double defaultWidth, double a, double b, int segments) {
+        LineRenderer renderer = new LineRenderer(name, defaultWidth, a, b, segments);
+        if (renderer.active) {
+            logger.fine("using %s", renderer);
+            renderers[type] = renderer;
+        } else {
+            logger.fine("%s not found", renderer);
+            renderers[type] = null;
+        }
+    }
+
+    LineRenderer(String name, double width, double a, double b, int segments) {
         texture = MCPatcherUtils.TEXTURE_PACK_PREFIX + "item/" + name.toLowerCase() + ".png";
-        this.defaultWidth = defaultWidth;
-        this.a = a;
-        this.b = b;
-        this.segments = segments;
-    }
-
-    void reset1() {
-        active = enable && TexturePackAPI.hasResource(texture);
-        width = defaultWidth;
-        if (active) {
-            Properties properties = TexturePackAPI.getProperties(LINE_PROPERTIES);
-            width = MCPatcherUtils.getDoubleProperty(properties, name + ".width", defaultWidth);
-        }
+        active = TexturePackAPI.hasResource(texture);
+        Properties properties = TexturePackAPI.getProperties(LINE_PROPERTIES);
+        this.width = MCPatcherUtils.getDoubleProperty(properties, name + ".width", width);
+        this.a = MCPatcherUtils.getDoubleProperty(properties, name + ".a", a);
+        this.b = MCPatcherUtils.getDoubleProperty(properties, name + ".b", b);
+        this.segments = MCPatcherUtils.getIntProperty(properties, name + ".segments", segments);
     }
 
     boolean render(double x, double y, double z, double dx, double dy, double dz) {
-        if (!active) {
-            return false;
-        }
         TexturePackAPI.bindTexture(texture);
         Tessellator tessellator = Tessellator.instance;
         tessellator.startDrawingQuads();
