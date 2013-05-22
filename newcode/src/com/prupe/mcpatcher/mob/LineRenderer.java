a8aa788fe5065b7970d11917ef333ec56a49576d
Paul Rupe <prupe@nc.rr.com>
2013-05-22 18:39:13 -0400

    debug keys to adjust width

--- newcode/src/com/prupe/mcpatcher/mob/LineRenderer.java
+++ newcode/src/com/prupe/mcpatcher/mob/LineRenderer.java
@@ -1,10 +1,8 @@
 package com.prupe.mcpatcher.mob;
 
-import com.prupe.mcpatcher.Config;
-import com.prupe.mcpatcher.MCLogger;
-import com.prupe.mcpatcher.MCPatcherUtils;
-import com.prupe.mcpatcher.TexturePackAPI;
+import com.prupe.mcpatcher.*;
 import net.minecraft.src.Tessellator;
+import org.lwjgl.input.Keyboard;
 
 import java.util.Properties;
 
@@ -12,6 +10,7 @@ public class LineRenderer {
     private static final MCLogger logger = MCLogger.getLogger(MCPatcherUtils.RANDOM_MOBS);
 
     private static final String LINE_PROPERTIES = MCPatcherUtils.TEXTURE_PACK_PREFIX + "item/line.properties";
+    private static final double D_WIDTH = 1.0 / 1024.0;
 
     private static final boolean enable = Config.getBoolean(MCPatcherUtils.RANDOM_MOBS, "leashLine", true);
     private static final LineRenderer[] renderers = new LineRenderer[2];
@@ -22,6 +21,9 @@ public class LineRenderer {
     private final double b;
     private final int segments;
     private final boolean active;
+    private final InputHandler keyboard;
+
+    private double extraWidth;
 
     public static boolean renderLine(int type, double x, double y, double z, double dx, double dy, double dz) {
         LineRenderer renderer = renderers[type];
@@ -54,15 +56,34 @@ public class LineRenderer {
         this.a = MCPatcherUtils.getDoubleProperty(properties, name + ".a", a);
         this.b = MCPatcherUtils.getDoubleProperty(properties, name + ".b", b);
         this.segments = MCPatcherUtils.getIntProperty(properties, name + ".segments", segments);
+        keyboard = new InputHandler(name, MCPatcherUtils.getBooleanProperty(properties, name + ".debug", true));
     }
 
     private boolean render(double x, double y, double z, double dx, double dy, double dz) {
+        if (keyboard.isKeyDown(Keyboard.KEY_MULTIPLY)) {
+            return false;
+        }
+        boolean changed = false;
+        if (keyboard.isKeyPressed(Keyboard.KEY_ADD)) {
+            extraWidth += D_WIDTH;
+            changed = true;
+        } else if (keyboard.isKeyPressed(Keyboard.KEY_SUBTRACT)) {
+            extraWidth -= D_WIDTH;
+            changed = true;
+        } else if (keyboard.isKeyPressed(Keyboard.KEY_DIVIDE)) {
+            extraWidth = 0.0;
+            changed = true;
+        }
         TexturePackAPI.bindTexture(texture);
         Tessellator tessellator = Tessellator.instance;
         tessellator.startDrawingQuads();
         double x0 = x;
         double y0 = y + a + b;
         double z0 = z;
+        double w = width + extraWidth;
+        if (changed) {
+            logger.info("%s", this);
+        }
         for (int i = 1; i <= segments; i++) {
             double s = i / (double) segments;
             double x1 = x + s * dx;
@@ -71,11 +92,11 @@ public class LineRenderer {
 
             tessellator.addVertexWithUV(x0, y0, z0, 0.0, 0.0);
             tessellator.addVertexWithUV(x1, y1, z1, 1.0, 0.0);
-            tessellator.addVertexWithUV(x1, y1 + width, z1, 1.0, 1.0);
-            tessellator.addVertexWithUV(x0, y0 + width, z0, 0.0, 1.0);
+            tessellator.addVertexWithUV(x1, y1 + w, z1, 1.0, 1.0);
+            tessellator.addVertexWithUV(x0, y0 + w, z0, 0.0, 1.0);
 
-            tessellator.addVertexWithUV(x0, y0 + width, z0, 0.0, 1.0);
-            tessellator.addVertexWithUV(x1, y1 + width, z1, 1.0, 1.0);
+            tessellator.addVertexWithUV(x0, y0 + w, z0, 0.0, 1.0);
+            tessellator.addVertexWithUV(x1, y1 + w, z1, 1.0, 1.0);
             tessellator.addVertexWithUV(x1, y1, z1, 1.0, 0.0);
             tessellator.addVertexWithUV(x0, y0, z0, 0.0, 0.0);
 
@@ -90,6 +111,6 @@ public class LineRenderer {
 
     @Override
     public String toString() {
-        return "LineRenderer{" + texture + ", " + width + "}";
+        return "LineRenderer{" + texture + ", " + (width + extraWidth) + "}";
     }
 }
