e37cd3b9831f5f4ab5b25cf051d2fe610db4b0d4
Paul Rupe <prupe@nc.rr.com>
2013-05-30 18:24:11 -0400

    md5sum for 13w22a

--- src/com/prupe/mcpatcher/MinecraftVersion.java
+++ src/com/prupe/mcpatcher/MinecraftVersion.java
@@ -217,6 +217,7 @@ final public class MinecraftVersion implements Comparable<MinecraftVersion> {
             addKnownVersion("13w19a", "4c2ecdebff5f2c7b41e423c58be67b98");
             addKnownVersion("13w21a", "6df497d9b4e425f54c6ce50d2fd5f68f");
             addKnownVersion("13w21b", "b1fe1a3ccccf8bf032ed6c73d5f31ee6");
+            addKnownVersion("13w22a", "bd610b1dc3bb9671dc72391db5918ffa");
 
             for (int i = 0; i < versionOrdering.size(); i++) {
                 MinecraftVersion a = versionOrdering.get(i);
