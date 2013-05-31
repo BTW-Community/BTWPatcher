01b0087b135d405c749f1bbdd4f89a405685a4d9
Paul Rupe <prupe@nc.rr.com>
2013-05-30 20:50:42 -0400

    use potion names in replacement pngs

--- newcode/src/com/prupe/mcpatcher/cit/CITUtils.java
+++ newcode/src/com/prupe/mcpatcher/cit/CITUtils.java
@@ -21,30 +21,31 @@ public class CITUtils {
     static final int ITEM_ID_COMPASS = 345;
     static final int ITEM_ID_CLOCK = 347;
     static final int ITEM_ID_POTION = 373;
+    static final int ITEM_ID_GLASS_BOTTLE = 374;
     static final int ITEM_ID_ENCHANTED_BOOK = 403;
 
-    private static final int[][] POTION_EFFECT_BITS = new int[][]{
-        {0xffff, 0},  // 0:  water
-        {0x401f, 2},  // 1:  move speed
-        {0x401f, 10}, // 2:  move slow
-        null,         // 3:  dig speed
-        null,         // 4:  dig slow
-        {0x401f, 9},  // 5:  damage boost
-        {0x401f, 5},  // 6:  heal
-        {0x401f, 12}, // 7:  harm
-        null,         // 8:  jump
-        null,         // 9:  confusion
-        {0x401f, 1},  // 10: regeneration
-        null,         // 11: resistance
-        {0x401f, 3},  // 12: fire resistance
-        null,         // 13: water breathing
-        {0x401f, 14}, // 14: invisibility
-        null,         // 15: blindness
-        {0x401f, 6},  // 16: night vision
-        null,         // 17: hunger
-        {0x401f, 8},  // 18: weakness
-        {0x401f, 4},  // 19: poison
-        null,         // 20: wither
+    private static final int[] POTION_EFFECTS = new int[]{
+        -1, // 0:  none
+        2,  // 1:  moveSpeed
+        10, // 2:  moveSlowdown
+        -1, // 3:  digSpeed
+        -1, // 4:  digSlowDown
+        9,  // 5:  damageBoost
+        5,  // 6:  heal
+        12, // 7:  harm
+        -1, // 8:  jump
+        -1, // 9:  confusion
+        1,  // 10: regeneration
+        -1, // 11: resistance
+        3,  // 12: fireResistance
+        -1, // 13: waterBreathing
+        14, // 14: invisibility
+        -1, // 15: blindness
+        6,  // 16: nightVision
+        -1, // 17: hunger
+        8,  // 18: weakness
+        4,  // 19: poison
+        -1, // 20: wither
     };
 
     private static final boolean enableItems = Config.getBoolean(MCPatcherUtils.CUSTOM_ITEM_TEXTURES, "items", true);
@@ -105,8 +106,7 @@ public class CITUtils {
                         registerOverride(ItemOverride.create(path));
                     }
                     if (enableItems) {
-                        registerPotions(MCPatcherUtils.TEXTURE_PACK_PREFIX + "cit/custom_potion_", 0, false);
-                        registerPotions(MCPatcherUtils.TEXTURE_PACK_PREFIX + "cit/custom_splash_potion_", 1, true);
+                        registerPotions();
                     }
                 }
             }
@@ -198,38 +198,121 @@ public class CITUtils {
                 return list;
             }
 
-            private void registerPotions(String prefix, int start, boolean splash) {
-                for (int id = start; id < POTION_EFFECT_BITS.length; id++) {
-                    String path = prefix + id + ".png";
-                    if (TexturePackAPI.hasResource(path)) {
-                        Properties properties = new Properties();
-                        properties.setProperty("type", "item");
-                        properties.setProperty("matchItems", "" + ITEM_ID_POTION);
+            private String getPotionPath(String name, boolean splash) {
+                return MCPatcherUtils.TEXTURE_PACK_PREFIX + "cit/custom_potion_" +
+                    (splash ? "splash_" : "") + name + ".png";
+            }
 
-                        if (POTION_EFFECT_BITS[id] == null) {
-                            if (splash) {
-                                continue;
-                            }
-                            properties.setProperty("nbt.CustomPotionEffects.0.Id", "" + id);
-                        } else {
-                            int mask = POTION_EFFECT_BITS[id][0];
-                            int damage = POTION_EFFECT_BITS[id][1];
+            private void registerPotions() {
+                String path = getPotionPath("water", false);
+                if (TexturePackAPI.hasResource(path)) {
+                    registerVanillaPotion(path, ITEM_ID_POTION, 0, 0xffff);
+                }
+                path = getPotionPath("empty", false);
+                if (TexturePackAPI.hasResource(path)) {
+                    registerVanillaPotion(path, ITEM_ID_GLASS_BOTTLE, 0, 0);
+                }
+                registerPotionsByEffect(false);
+                registerPotionsByEffect(true);
+                path = getPotionPath("other", false);
+                if (TexturePackAPI.hasResource(path)) {
+                    registerVanillaPotion(path, ITEM_ID_POTION, 0, 0x401f);
+                }
+                path = getPotionPath("other", true);
+                if (TexturePackAPI.hasResource(path)) {
+                    registerVanillaPotion(path, ITEM_ID_POTION, 0x4000, 0x400f);
+                }
+            }
+
+            private void registerPotionsByEffect(boolean splash) {
+                for (int effect = 0; effect < Potion.potionTypes.length; effect++) {
+                    if (Potion.potionTypes[effect] == null) {
+                        continue;
+                    }
+                    String path = getPotionPath(Potion.potionTypes[effect].getName().replaceFirst("^potion\\.", ""), splash);
+                    if (TexturePackAPI.hasResource(path)) {
+                        if (effect < POTION_EFFECTS.length && POTION_EFFECTS[effect] >= 0) {
+                            int damage = POTION_EFFECTS[effect];
                             if (splash) {
                                 damage |= 0x4000;
                             }
-                            properties.setProperty("damage", "" + damage);
-                            properties.setProperty("damageMask", "" + mask);
+                            registerVanillaPotion(path, damage, 0x400f);
                         }
-
-                        properties.setProperty("texture", path);
-                        properties.setProperty("layer", "0");
-                        registerOverride(new ItemOverride(path, properties));
-
-                        properties.setProperty("texture", "blank");
-                        properties.setProperty("layer", "1");
-                        registerOverride(new ItemOverride(path, properties));
+                        if (!splash) {
+                            registerCustomPotion(path, effect);
+                        }
                     }
                 }
+            }
+
+            private void registerMundanePotion(String name, int damage) {
+                registerMundanePotion(name, damage, false);
+                registerMundanePotion(name, damage | 0x4000, true);
+            }
+
+            private void registerMundanePotion(String name, int damage, boolean splash) {
+                String path = getPotionPath(name, splash);
+                if (TexturePackAPI.hasResource(path)) {
+                    registerVanillaPotion(path, damage, 0x403f);
+                }
+            }
+
+            private void registerOtherPotions(boolean splash) {
+                String path = getPotionPath("other", splash);
+                if (TexturePackAPI.hasResource(path)) {
+                    Properties properties = new Properties();
+                    properties.setProperty("type", "item");
+                    properties.setProperty("matchItems", String.valueOf(ITEM_ID_POTION));
+                    StringBuilder sb = new StringBuilder();
+                    for (int i : new int[]{0, 7, 11, 13, 15, 16, 23, 27, 29, 31, 32, 39, 43, 45, 47, 48, 55, 59, 61, 63}) {
+                        if (splash) {
+                            i |= 0x4000;
+                        }
+                        sb.append(' ').append(i);
+                    }
+                    properties.setProperty("damage", sb.toString().trim());
+                    properties.setProperty("damageMask", String.valueOf(0x403f));
+                    properties.setProperty("texture", path);
+                    properties.setProperty("layer", "0");
+                    registerOverride(new ItemOverride("(none)", properties));
+
+                    properties.setProperty("texture", "blank");
+                    properties.setProperty("layer", "1");
+                    registerOverride(new ItemOverride("(none)", properties));
+                }
+            }
+
+            private void registerVanillaPotion(String path, int damage, int mask) {
+                registerVanillaPotion(path, ITEM_ID_POTION, damage, mask);
+            }
+
+            private void registerVanillaPotion(String path, int itemID, int damage, int mask) {
+                Properties properties = new Properties();
+                properties.setProperty("type", "item");
+                properties.setProperty("matchItems", String.valueOf(itemID));
+                properties.setProperty("damage", String.valueOf(damage));
+                properties.setProperty("damageMask", String.valueOf(mask));
+                properties.setProperty("texture", path);
+                properties.setProperty("layer", "0");
+                registerOverride(new ItemOverride("(none)", properties));
+
+                properties.setProperty("texture", "blank");
+                properties.setProperty("layer", "1");
+                registerOverride(new ItemOverride("(none)", properties));
+            }
+
+            private void registerCustomPotion(String path, int effect) {
+                Properties properties = new Properties();
+                properties.setProperty("type", "item");
+                properties.setProperty("matchItems", String.valueOf(ITEM_ID_POTION));
+                properties.setProperty("nbt.CustomPotionEffects.0.Id", String.valueOf(effect));
+                properties.setProperty("texture", path);
+                properties.setProperty("layer", "0");
+                registerOverride(new ItemOverride("(none)", properties));
+
+                properties.setProperty("texture", "blank");
+                properties.setProperty("layer", "1");
+                registerOverride(new ItemOverride("(none)", properties));
             }
         });
     }
