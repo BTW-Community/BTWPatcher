9e3327bc7c9836f3f23c45c26269226db51c995d
Paul Rupe <prupe@nc.rr.com>
2013-06-08 06:52:36 -0400

    version 3.0.4_01

--- src/com/prupe/mcpatcher/MCPatcher.java
+++ src/com/prupe/mcpatcher/MCPatcher.java
@@ -29,7 +29,7 @@ final public class MCPatcher {
     /**
      * MCPatcher patch level
      */
-    public static final int PATCH_VERSION = 0;
+    public static final int PATCH_VERSION = 1;
     /**
      * MCPatcher beta version if > 0
      */
