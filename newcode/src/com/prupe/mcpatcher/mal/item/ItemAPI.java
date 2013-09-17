734682c5e0b2a0143924bdcd1c07c983e5304fa3
Paul Rupe <prupe@nc.rr.com>
2013-09-17 18:59:48 -0400

    parse items by name or id

--- newcode/src/com/prupe/mcpatcher/mal/item/ItemAPI.java
+++ newcode/src/com/prupe/mcpatcher/mal/item/ItemAPI.java
@@ -3,66 +3,87 @@ package com.prupe.mcpatcher.mal.item;
 import com.prupe.mcpatcher.MAL;
 import net.minecraft.src.Item;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 abstract public class ItemAPI {
     private static final ItemAPI instance = MAL.newInstance(ItemAPI.class, "item");
 
     public static Item parseItemName(String name) {
-        return null; // TODO
+        if (name.matches("\\d+")) {
+            int id = Integer.parseInt(name);
+            return instance.getItemById_Impl(id);
+        }
+        name = instance.getCanonicalName_Impl(name);
+        for (Item item : getAllItems()) {
+            if (name.equals(getItemName(item))) {
+                return item;
+            }
+        }
+        return null;
     }
 
-    public static Item getItemById(int id) {
-        return instance.getItemById_Impl(id);
+    public static String getItemName(Item item) {
+        return item.getItemName();
     }
 
-    @Deprecated
-    public static int getItemId(Item item) {
-        return instance.getItemId_Impl(item);
+    public static List<Item> getAllItems() {
+        List<Item> items = new ArrayList<Item>();
+        for (Item item : instance.getAllItems_Impl()) {
+            if (item != null && !items.contains(item)) {
+                items.add(item);
+            }
+        }
+        return items;
     }
 
-    public static int getNumItems() {
-        return instance.getNumItems_Impl();
-    }
+    abstract protected List<Item> getAllItems_Impl();
+
+    abstract protected String getCanonicalName_Impl(String name);
 
     abstract protected Item getItemById_Impl(int id);
-
-    abstract protected int getItemId_Impl(Item item);
-
-    abstract protected int getNumItems_Impl();
 
     ItemAPI() {
     }
 
     final private static class V1 extends ItemAPI {
         @Override
+        protected List<Item> getAllItems_Impl() {
+            return Arrays.asList(Item.itemsList);
+        }
+
+        @Override
+        protected String getCanonicalName_Impl(String name) {
+            if (name.startsWith("minecraft:")) {
+                name = name.substring(10);
+            }
+            return name;
+        }
+
+        @Override
         protected Item getItemById_Impl(int id) {
-            return Item.itemsList[id];
-        }
-
-        @Override
-        protected int getItemId_Impl(Item item) {
-            return item.itemID;
-        }
-
-        @Override
-        protected int getNumItems_Impl() {
-            return Item.itemsList.length;
+            return id >= 0 && id < Item.itemsList.length ? Item.itemsList[id] : null;
         }
     }
 
     final private static class V2 extends ItemAPI {
         @Override
+        protected List<Item> getAllItems_Impl() {
+            return Item.itemRegistry.getAll();
+        }
+
+        @Override
+        protected String getCanonicalName_Impl(String name) {
+            if (!name.contains(":")) {
+                name = "minecraft:" + name;
+            }
+            return name;
+        }
+
+        @Override
         protected Item getItemById_Impl(int id) {
             return Item.itemRegistry.getById(id);
-        }
-
-        @Override
-        protected int getItemId_Impl(Item item) {
-            return Item.itemRegistry.getId(item);
-        }
-
-        @Override
-        protected int getNumItems_Impl() {
-            return 32000;
         }
     }
 }
