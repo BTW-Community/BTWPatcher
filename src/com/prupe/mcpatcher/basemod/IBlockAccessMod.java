8623239797317a20be636ae6979d186557b63645
Paul Rupe <prupe@nc.rr.com>
2014-01-13 19:59:41 -0500

    simplify IBlockAccess method list

--- src/com/prupe/mcpatcher/basemod/IBlockAccessMod.java
+++ src/com/prupe/mcpatcher/basemod/IBlockAccessMod.java
@@ -3,8 +3,11 @@ package com.prupe.mcpatcher.basemod;
 import com.prupe.mcpatcher.InterfaceMethodRef;
 import com.prupe.mcpatcher.Mod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Matches IBlockAccess interface and maps getBlockId, getBlockMetadata methods.
+ * Matches IBlockAccess interface and maps all of its methods.
  */
 public class IBlockAccessMod extends com.prupe.mcpatcher.ClassMod {
     protected final boolean haveBlockRegistry;
@@ -16,33 +19,36 @@ public class IBlockAccessMod extends com.prupe.mcpatcher.ClassMod {
         methodsRemoved = Mod.getMinecraftVersion().compareTo("13w38b") >= 0;
         final String d = PositionMod.getDescriptor();
 
-        addClassSignature(new InterfaceSignature(
-            haveBlockRegistry ?
-                new InterfaceMethodRef(getDeobfClass(), "getBlock", "(" + d + ")LBlock;") :
-                new InterfaceMethodRef(getDeobfClass(), "getBlockId", "(III)I"),
-            new InterfaceMethodRef(getDeobfClass(), "getBlockTileEntity", "(" + d + ")LTileEntity;"),
-            new InterfaceMethodRef(getDeobfClass(), "getLightBrightnessForSkyBlocks", "(" + d + "I)I"),
-            methodsRemoved ?
-                null : new InterfaceMethodRef(getDeobfClass(), "getBrightness", "(IIII)F"),
-            methodsRemoved ?
-                null : new InterfaceMethodRef(getDeobfClass(), "getLightBrightness", "(III)F"),
-            new InterfaceMethodRef(getDeobfClass(), "getBlockMetadata", "(" + d + ")I"),
-            methodsRemoved ?
-                null : new InterfaceMethodRef(getDeobfClass(), "getBlockMaterial", "(III)LMaterial;"),
-            methodsRemoved ?
-                null : new InterfaceMethodRef(getDeobfClass(), "isBlockOpaqueCube", "(III)Z"),
-            methodsRemoved ?
-                null : new InterfaceMethodRef(getDeobfClass(), "isBlockNormalCube", "(III)Z"),
-            new InterfaceMethodRef(getDeobfClass(), "isAirBlock", "(" + d + ")Z"),
-            new InterfaceMethodRef(getDeobfClass(), "getBiomeGenAt", "(" + PositionMod.getDescriptorIKOnly() + ")LBiomeGenBase;"),
-            new InterfaceMethodRef(getDeobfClass(), "getHeight", "()I"),
-            new InterfaceMethodRef(getDeobfClass(), "extendedLevelsInChunkCache", "()Z"),
-            methodsRemoved ?
-                null : new InterfaceMethodRef(getDeobfClass(), "doesBlockHaveSolidTopSurface", "(III)Z"),
-            PositionMod.havePositionClass() ?
-                null : new InterfaceMethodRef(getDeobfClass(), "getWorldVec3Pool", "()LVec3Pool;"),
-            new InterfaceMethodRef(getDeobfClass(), "isBlockProvidingPowerTo",
-                PositionMod.havePositionClass() ? "(LPosition;LDirection;)I" : "(IIII)I")
-        ).setInterfaceOnly(true));
+        List<InterfaceMethodRef> tmp = new ArrayList<InterfaceMethodRef>();
+        if (haveBlockRegistry) {
+            tmp.add(new InterfaceMethodRef(getDeobfClass(), "getBlock", "(" + d + ")LBlock;"));
+        } else {
+            tmp.add(new InterfaceMethodRef(getDeobfClass(), "getBlockId", "(" + d + ")I"));
+        }
+        tmp.add(new InterfaceMethodRef(getDeobfClass(), "getBlockTileEntity", "(" + d + ")LTileEntity;"));
+        tmp.add(new InterfaceMethodRef(getDeobfClass(), "getLightBrightnessForSkyBlocks", "(" + d + "I)I"));
+        if (!methodsRemoved) {
+            tmp.add(new InterfaceMethodRef(getDeobfClass(), "getBrightness", "(IIII)F"));
+            tmp.add(new InterfaceMethodRef(getDeobfClass(), "getLightBrightness", "(III)F"));
+        }
+        tmp.add(new InterfaceMethodRef(getDeobfClass(), "getBlockMetadata", "(" + d + ")I"));
+        if (!methodsRemoved) {
+            tmp.add(new InterfaceMethodRef(getDeobfClass(), "getBlockMaterial", "(III)LMaterial;"));
+            tmp.add(new InterfaceMethodRef(getDeobfClass(), "isBlockOpaqueCube", "(III)Z"));
+            tmp.add(new InterfaceMethodRef(getDeobfClass(), "isBlockNormalCube", "(III)Z"));
+        }
+        tmp.add(new InterfaceMethodRef(getDeobfClass(), "isAirBlock", "(" + d + ")Z"));
+        tmp.add(new InterfaceMethodRef(getDeobfClass(), "getBiomeGenAt", "(" + PositionMod.getDescriptorIKOnly() + ")LBiomeGenBase;"));
+        tmp.add(new InterfaceMethodRef(getDeobfClass(), "getHeight", "()I"));
+        tmp.add(new InterfaceMethodRef(getDeobfClass(), "extendedLevelsInChunkCache", "()Z"));
+        if (!methodsRemoved) {
+            tmp.add(new InterfaceMethodRef(getDeobfClass(), "doesBlockHaveSolidTopSurface", "(III)Z"));
+        }
+        if (!PositionMod.havePositionClass()) {
+            tmp.add(new InterfaceMethodRef(getDeobfClass(), "getWorldVec3Pool", "()LVec3Pool;"));
+        }
+        tmp.add(new InterfaceMethodRef(getDeobfClass(), "isBlockProvidingPowerTo", "(" + d + DirectionMod.getDescriptor() + ")I"));
+
+        addClassSignature(new InterfaceSignature(tmp.toArray(new InterfaceMethodRef[tmp.size()])).setInterfaceOnly(true));
     }
 }
