2e2036cbf7e4e9e1062796e6e7a04a9f02b872e9
Paul Rupe <prupe@nc.rr.com>
2014-03-07 05:22:19 -0500

    make Direction stub an enum

--- stubs/src/net/minecraft/src/Direction.java
+++ stubs/src/net/minecraft/src/Direction.java
@@ -1,14 +1,10 @@
 package net.minecraft.src;
 
-public class Direction {
-    public static Direction DOWN;
-    public static Direction UP;
-    public static Direction NORTH;
-    public static Direction SOUTH;
-    public static Direction WEST;
-    public static Direction EAST;
-
-    public int getID() {
-        return 0;
-    }
+public enum Direction {
+    DOWN,
+    UP,
+    NORTH,
+    SOUTH,
+    WEST,
+    EAST
 }
