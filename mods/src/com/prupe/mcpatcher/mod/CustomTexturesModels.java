1936eed27ff23a16ae18bd86a10c404620d582db
Paul Rupe <prupe@nc.rr.com>
2014-08-12 19:33:07 -0400

    map RenderBlocks18 methods

--- mods/src/com/prupe/mcpatcher/mod/CustomTexturesModels.java
+++ mods/src/com/prupe/mcpatcher/mod/CustomTexturesModels.java
@@ -1,10 +1,12 @@
 package com.prupe.mcpatcher.mod;
 
-import com.prupe.mcpatcher.MCPatcherUtils;
-import com.prupe.mcpatcher.Mod;
-import com.prupe.mcpatcher.basemod.BlockMod;
-import com.prupe.mcpatcher.basemod.ItemMod;
+import com.prupe.mcpatcher.*;
+import com.prupe.mcpatcher.basemod.*;
 import com.prupe.mcpatcher.mal.TexturePackAPIMod;
+
+import static com.prupe.mcpatcher.BinaryRegex.*;
+import static com.prupe.mcpatcher.BytecodeMatcher.*;
+import static javassist.bytecode.Opcode.*;
 
 public class CustomTexturesModels extends Mod {
     public CustomTexturesModels() {
@@ -20,7 +22,14 @@ public class CustomTexturesModels extends Mod {
         addDependency(MCPatcherUtils.NBT_API_MOD);
         addDependency(MCPatcherUtils.BIOME_API_MOD);
 
+        ResourceLocationMod.setup(this);
+        addClassMod(new IBlockAccessMod(this));
+        addClassMod(new TessellatorMod(this));
+        addClassMod(new TessellatorFactoryMod(this));
+        addClassMod(new BiomeGenBaseMod(this));
+
         addClassMod(new BlockMod(this));
+        addClassMod(new RenderBlocks18Mod());
 
         addClassMod(new ItemMod(this));
 
@@ -37,5 +46,83 @@ public class CustomTexturesModels extends Mod {
             MCPatcherUtils.CONNECTED_TEXTURES,
             MCPatcherUtils.CUSTOM_ITEM_TEXTURES
         };
+    }
+
+    private class RenderBlocks18Mod extends ClassMod {
+        RenderBlocks18Mod() {
+            final MethodRef renderBlock = new MethodRef(getDeobfClass(), "renderBlock", "(LIBlockAccess;LIModel;LIBlockState;LPosition;LTessellator;Z)Z");
+            final MethodRef renderBlockAO = new MethodRef(getDeobfClass(), "renderBlockAO", "(LIBlockAccess;LIModel;LBlock;LPosition;LTessellator;Z)Z");
+            final MethodRef renderBlockNonAO = new MethodRef(getDeobfClass(), "renderBlockNonAO", "(LIBlockAccess;LIModel;LBlock;LPosition;LTessellator;Z)Z");
+            final MethodRef renderFaceAO = new MethodRef(getDeobfClass(), "renderFaceAO", "(LIBlockAccess;LBlock;LPosition;LTessellator;Ljava/util/List;[FLjava/util/BitSet;LRenderBlocks18Inner;)V");
+            final MethodRef renderFaceNonAO = new MethodRef(getDeobfClass(), "renderFaceNonAO", "(LIBlockAccess;LBlock;LPosition;LDirection;IZLTessellator;Ljava/util/List;Ljava/util/BitSet;)V");
+
+            addClassSignature(new ConstSignature(0xf000f));
+
+            addClassSignature(new BytecodeSignature() {
+                {
+                    setMethod(renderBlock);
+                }
+
+                @Override
+                public String getMatchExpression() {
+                    return buildExpression(
+                        push("Using AO")
+                    );
+                }
+            });
+
+            addClassSignature(new BytecodeSignature() {
+                {
+                    setMethod(renderBlockAO);
+                    addXref(1, renderFaceAO);
+                }
+
+                @Override
+                public String getMatchExpression() {
+                    return buildExpression(
+                        push(0xf000f),
+
+                        any(0, 500),
+
+                        // this.renderFaceAO(...);
+                        ALOAD_0,
+                        ALOAD_1,
+                        ALOAD_3,
+                        ALOAD, 4,
+                        ALOAD, 5,
+                        anyALOAD,
+                        anyALOAD,
+                        anyALOAD,
+                        anyALOAD,
+                        captureReference(INVOKESPECIAL)
+                    );
+                }
+            });
+
+            addClassSignature(new BytecodeSignature() {
+                {
+                    setMethod(renderBlockNonAO);
+                    addXref(1, renderFaceNonAO);
+                }
+
+                @Override
+                public String getMatchExpression() {
+                    return buildExpression(
+                        // this.renderFaceNonAO(...)
+                        ALOAD_0,
+                        ALOAD_1,
+                        ALOAD_3,
+                        ALOAD, 4,
+                        ACONST_NULL,
+                        push(-1),
+                        push(1),
+                        ALOAD, 5,
+                        anyALOAD,
+                        anyALOAD,
+                        captureReference(INVOKESPECIAL)
+                    );
+                }
+            });
+        }
     }
 }
