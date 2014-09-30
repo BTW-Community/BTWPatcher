7f97b149824c0e42ecee355427521a65b646a641
Paul Rupe <prupe@nc.rr.com>
2014-09-29 20:17:04 -0400

    translate faces top,bottom -> up,down, etc.

--- newcode/src/com/prupe/mcpatcher/ctm/BlockFaceMatcher.java
+++ newcode/src/com/prupe/mcpatcher/ctm/BlockFaceMatcher.java
@@ -16,7 +16,10 @@ abstract public class BlockFaceMatcher {
         if (!MCPatcherUtils.isNullOrEmpty(propertyValue)) {
             String[] values = propertyValue.toLowerCase().split("\\s+");
             try {
-                return matcherClass.getDeclaredConstructor(String[].class).newInstance((Object) values);
+                BlockFaceMatcher matcher = matcherClass.getDeclaredConstructor(String[].class).newInstance((Object) values);
+                if (!matcher.isAll()) {
+                    return matcher;
+                }
             } catch (Throwable e) {
                 e.printStackTrace();
             }
@@ -26,27 +29,29 @@ abstract public class BlockFaceMatcher {
 
     abstract public boolean match(RenderBlockState renderBlockState);
 
+    abstract protected boolean isAll();
+
     final public static class V1 extends BlockFaceMatcher {
         private final int faces;
 
-        protected V1(String[] value) {
+        protected V1(String[] values) {
             int flags = 0;
-            for (String val : value) {
-                if (val.equals("bottom")) {
+            for (String face : values) {
+                if (face.equals("bottom")) {
                     flags |= (1 << BOTTOM_FACE);
-                } else if (val.equals("top")) {
+                } else if (face.equals("top")) {
                     flags |= (1 << TOP_FACE);
-                } else if (val.equals("north")) {
+                } else if (face.equals("north")) {
                     flags |= (1 << NORTH_FACE);
-                } else if (val.equals("south")) {
+                } else if (face.equals("south")) {
                     flags |= (1 << SOUTH_FACE);
-                } else if (val.equals("east")) {
+                } else if (face.equals("east")) {
                     flags |= (1 << EAST_FACE);
-                } else if (val.equals("west")) {
+                } else if (face.equals("west")) {
                     flags |= (1 << WEST_FACE);
-                } else if (val.equals("side") || val.equals("sides")) {
+                } else if (face.equals("side") || face.equals("sides")) {
                     flags |= (1 << NORTH_FACE) | (1 << SOUTH_FACE) | (1 << EAST_FACE) | (1 << WEST_FACE);
-                } else if (val.equals("all")) {
+                } else if (face.equals("all")) {
                     flags = -1;
                 }
             }
@@ -58,18 +63,48 @@ abstract public class BlockFaceMatcher {
             int face = renderBlockState.getTextureFace();
             return face >= 0 && (faces & (1 << face)) != 0;
         }
+
+        @Override
+        protected boolean isAll() {
+            return faces == -1;
+        }
     }
 
     final public static class V2 extends BlockFaceMatcher {
         private final Set<String> faces = new HashSet<String>();
+        private boolean all;
 
-        protected V2(String[] value) {
-            Collections.addAll(faces, value);
+        protected V2(String[] values) {
+            for (String face : values) {
+                if (MCPatcherUtils.isNullOrEmpty(face)) {
+                    continue;
+                }
+                faces.add(face);
+                if (face.equals("bottom")) {
+                    faces.add("down");
+                } else if (face.equals("top")) {
+                    faces.add("up");
+                } else if (face.equals("side") || face.equals("sides")) {
+                    faces.add("north");
+                    faces.add("south");
+                    faces.add("west");
+                    faces.add("east");
+                } else if (face.equals("all")) {
+                    all = true;
+                    break;
+                }
+            }
+            Collections.addAll(faces, values);
         }
 
         @Override
         public boolean match(RenderBlockState renderBlockState) {
             return faces.contains(renderBlockState.getTextureFaceName());
+        }
+
+        @Override
+        protected boolean isAll() {
+            return all;
         }
     }
 }
