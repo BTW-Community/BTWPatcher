0a1f216b54d7e8f6220cc38d56afaf3ded7a0507
Paul Rupe <prupe@nc.rr.com>
2015-01-31 05:39:26 -0500

    add inner class regexes to test output filter

--- testfilter.pl
+++ testfilter.pl
@@ -2,15 +2,15 @@
 
 use strict;
 
-s/\b[a-z]+\.class/xx.class/g;
-s/L[a-z]+;/Lxx;/g;
-s/ [a-z]+/ xx/g if /multiple matches: /;
-s/\b[a-z_]+(\(\S*\))/xx$1/g;
+s/\b[a-z]+(\$[a-z0-9]+)*\.class/xx.class/g;
+s/L[a-z]+(\$[a-z0-9]+)*;/Lxx;/g;
+s/ [a-z]+(\$[a-z0-9]+)*/ xx/g if /multiple matches: /;
+s/\b[a-z_]+(\$[a-z0-9]+)*(\(\S*\))/xx$1/g;
 
 s/(matches|->) [a-zA-Z_]{1,3}\b/$1 xx/g if /field|string replace/;
 s/(matches|->) [a-zA-Z_]{1,3} (\w*\(\S*\)\S+)/$1 xx $2/g if /method|class ref/;
-s/-> [a-z\/M]+\.[a-zA-Z_<>]+ /-> xx.x /g if /(class|method|field) ref/;
-s/-> [a-z\/M]+ (<(cl)?init>)/-> xx $1/g if /class ref/;
+s/-> [a-z\/M]+(\$[a-z0-9]+)*\.[a-zA-Z_<>]+ /-> xx.x /g if /(class|method|field) ref/;
+s/-> [a-z\/M]+(\$[a-z0-9]+)* (<(cl)?init>)/-> xx $1/g if /class ref/;
 
 s/@[[:digit:]]+/@.../g;
 s/-> instruction \d+/-> instruction .../;
