05fb33d1b91c4fdfde2e077e68497f48a751f661
Paul Rupe <prupe@nc.rr.com>
2015-04-05 12:59:08 -0400

    updated test output

--- testfilter.pl
+++ testfilter.pl
@@ -10,6 +10,7 @@ s/\b[a-z_]+(\$[a-z0-9]+)*(\(\S*\))/xx$1/g;
 s/(matches|->) [a-zA-Z_]{1,3}\b/$1 xx/g if /field|string replace/;
 s/(matches|->) [a-zA-Z_]{1,3} (\w*\(\S*\)\S+)/$1 xx $2/g if /method|class ref/;
 s/-> [a-z\/M]+(\$[a-z0-9]+)*\.[a-zA-Z_<>]+ /-> xx.x /g if /(class|method|field) ref/;
+s/-> [a-z\/M]+(\$[a-z0-9]+)* /-> xx /g if /class ref/;
 s/-> [a-z\/M]+(\$[a-z0-9]+)* (<(cl)?init>)/-> xx $1/g if /class ref/;
 
 s/@[[:digit:]]+/@.../g;
